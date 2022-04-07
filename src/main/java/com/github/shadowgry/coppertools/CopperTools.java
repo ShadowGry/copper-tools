e7efac5bc32aa6473cbd3fd0cfe0b19a32686607
ShadowGry <sean.stephenson@iinet.net.au>
2022-04-07 11:05:51 +0800

    Customize mod information and restructure

---
+++ src/main/java/com/github/shadowgry/coppertools/CopperTools.java
@@ -0,0 +1,25 @@
+/*
+ * This file is part of Copper Tools.
+ * Copyright (C) 2022  ShadowGry
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.github.shadowgry.coppertools;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CopperTools.MOD_ID)
+public class CopperTools {
+    public static final String MOD_ID = "coppertools";
+}
