905f57cba5ae23214387cef2eb6a74850f4f3790
ShadowGry <sean.stephenson@iinet.net.au>
2022-05-16 02:40:50 +0800

    Generalize oxidization for copper pickaxes
    Items cannot be called with RegsitryObject<T>#get until said items have
    been registered, which occurs later during mod loading than when
    ModEventHandler#onBreakEvent is subscribed. Therefore, the map which
    keeps #onBreakEvent quite simple is not initialized until the first
    BreakEvent occurs in-game.

--- src/main/java/com/github/shadowgry/coppertools/client/events/ModEventHandler.java
+++ src/main/java/com/github/shadowgry/coppertools/client/events/ModEventHandler.java
@@ -17,42 +17,137 @@
  */
 package com.github.shadowgry.coppertools.client.events;
 
-import com.github.shadowgry.coppertools.CopperTools;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.github.shadowgry.coppertools.common.items.ModItems;
+import com.github.shadowgry.coppertools.common.items.ModTiers;
 import com.mojang.logging.LogUtils;
 
 import org.slf4j.Logger;
 
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
-@Mod.EventBusSubscriber(modid = CopperTools.MOD_ID, bus = Bus.FORGE, value = Dist.CLIENT)
+/**
+ * Implements copper tool oxidizations.
+ */
 public class ModEventHandler {
     
     public static final Logger LOGGER = LogUtils.getLogger();
     
     /**
-     * Oxidizes a copper pickaxe into an exposed copper pickaxe after its 
-     * damage value reaches 50.
+     * Flag to denote if copper tool oxidization order has been specified.
+     */
+    private boolean isMapInitialized;
+    
+    /**
+     * Stores data to appropriately oxidize copper tools.
+     */
+    private Map<Item, OxidizeData> oxidizeData;
+    
+    public ModEventHandler() {
+        isMapInitialized  = false;
+        oxidizeData = null;
+    }
+    
+    /**
+     * Puts the copper pickaxe oxidization orderings into a map, for quick future lookup. Sets {@code isMapInitialized}
+     * to true, as this only needs to be run once.
+     * 
+     * TODO: Run earlier during mod loading, rather than on first BreakEvent. 
+     */
+    private void initializeMap() {
+        oxidizeData = new HashMap<>();
+        oxidizeData.put(ModItems.COPPER_PICKAXE.get().asItem(),
+                        new OxidizeData(ModItems.EXPOSED_COPPER_PICKAXE.get().getDefaultInstance(),
+                            ModTiers.COPPER.getUses()/4,
+                            ModTiers.EXPOSED_COPPER.getUses()/4));
+        oxidizeData.put(ModItems.EXPOSED_COPPER_PICKAXE.get().asItem(),
+                        new OxidizeData(ModItems.WEATHERED_COPPER_PICKAXE.get().getDefaultInstance(),
+                            ModTiers.EXPOSED_COPPER.getUses() * 1/2,
+                            ModTiers.WEATHERED_COPPER.getUses() * 1/2));
+        oxidizeData.put(ModItems.WEATHERED_COPPER_PICKAXE.get().asItem(),
+                        new OxidizeData(ModItems.OXIDIZED_COPPER_PICKAXE.get().getDefaultInstance(),
+                                ModTiers.WEATHERED_COPPER.getUses() * 3/4,
+                                ModTiers.OXIDIZED_COPPER.getUses() * 3/4));
+        isMapInitialized = true;
+    }
+    
+    /**
+     * Oxidizes copper pickaxes to the next stage when they are damaged enough.
      */
     @SubscribeEvent
-    public static void onBreakEvent(BreakEvent event) {
+    public void onBreakEvent(BreakEvent event) {
+        if(!isMapInitialized) {
+            initializeMap();
+        }
+        
         Player player = event.getPlayer();
         if(!player.isCreative()) {
             ItemStack item = player.getMainHandItem();
-            if(item.getItem() == ModItems.COPPER_PICKAXE.get().asItem()) {
-                if(item.getDamageValue() > 50) {
-                    ItemStack oxidizedTool = ModItems.EXPOSED_COPPER_PICKAXE.get().getDefaultInstance();
-                    oxidizedTool.setDamageValue(40);
-                    player.setItemInHand(InteractionHand.MAIN_HAND, oxidizedTool);
-                }
+            OxidizeData data = oxidizeData.get(item.getItem());
+
+            if(data != null && item.getDamageValue() > data.getOxidizeDamageValue()) {
+                ItemStack nextPickaxe = data.getNextTool();
+                nextPickaxe.setDamageValue(data.getStartDamageValue());
+                player.setItemInHand(InteractionHand.MAIN_HAND, nextPickaxe);
             }
+        }
+    }
+    
+    /**
+     * Stores information to appropriately oxidize a copper tool to the next tool. Copper tools oxidize in a linear way.
+     * The current (or previous tool) oxidizes to {@nextTool}, however, previous tool is not stored in this class - it
+     * is recommended to use a map to store it and map it to the appropriate instance of this class.
+     */
+    private class OxidizeData {
+        private ItemStack nextTool;
+        private int oxidizeDamageValue;
+        private int startDamageValue;
+        
+        /**
+         * Creates a new cache to store information about a particular copper tool oxidization.
+         * 
+         * @param nextTool the new tool the old one turns into
+         * @param oxidizeDamageValue the damage value required for the previous tool to oxidize into {@code nextTool}
+         * @param startDamageValue the initial damage of {@code nextTool}
+         */
+        OxidizeData(ItemStack nextTool, int oxidizeDamageValue, int startDamageValue) {
+            this.nextTool = nextTool;
+            this.oxidizeDamageValue = oxidizeDamageValue;
+            this.startDamageValue = startDamageValue;
+        }
+        
+        /**
+         * Returns the next tool the old one turns into.
+         * 
+         * @return the next tool the old one turns into.
+         */
+        ItemStack getNextTool() {
+            return this.nextTool;
+        }
+        
+        /**
+         * Returns the damage value required for the previous tool to oxidize into {@code nextTool}.
+         * 
+         * @return the damage value required for the previous tool to oxidize into {@code nextTool}
+         */
+        int getOxidizeDamageValue() {
+            return this.oxidizeDamageValue;
+        }
+        
+        /**
+         * Returns the initial damage of {@code nextTool}.
+         * 
+         * @return the initial damage of {@code nextTool}
+         */
+        int getStartDamageValue() {
+            return this.startDamageValue;
         }
     }
 }
